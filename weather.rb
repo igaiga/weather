a9a0f783c33e2bbdf7466e42cc5ae3457d16f7f6
Kuniaki IGARASHI <igaiga@gmail.com>
2013-05-12 16:38:12 +0900

    Decolate

--- weather.rb
+++ weather.rb
@@ -8,7 +8,6 @@ require 'date'
 require 'awesome_print'
 require 'pry'
 
-
 # im-kayac
 require 'im-kayac'
 require 'pit'
@@ -16,17 +15,31 @@ config_im_kayac = Pit.get 'im-kayac'
 user = config_im_kayac['user']
 password = config_im_kayac['pass']
 
-def parse(json)
-  h ={}
-  today_forecast = json["forecasts"].select{|day| day["dateLabel"] == '今日'}.first
-  h[:telop] = today_forecast["telop"]
-  h[:date] = today_forecast["date"]
-  h[:temperature_min] = today_forecast["temperature"]["min"]["celsius"] if today_forecast["temperature"]["min"]
-  h[:temperature_max] = today_forecast["temperature"]["max"]["celsius"] if today_forecast["temperature"]["max"]
+def parse_day_forecast(day_forecast)
+  h = {}
+  h[:telop] = day_forecast["telop"]
+  h[:date] = day_forecast["date"]
+  h[:temperature_min] = day_forecast["temperature"]["min"]["celsius"] if day_forecast["temperature"]["min"]
+  h[:temperature_max] = day_forecast["temperature"]["max"]["celsius"] if day_forecast["temperature"]["max"]
   h
 end
 
-def message(h)
+def parse(json)
+  forecasts = []
+  today_forecast = json["forecasts"].select{|day| day["dateLabel"] == '今日'}.first
+  tomorrow_forecast = json["forecasts"].select{|day| day["dateLabel"] == '明日'}.first
+  forecasts << parse_day_forecast(today_forecast)
+  forecasts << parse_day_forecast(tomorrow_forecast)
+  forecasts
+end
+
+def decolate(h)
+  replaces = [["晴れ"," ☀ "], ["晴"," ☀ "], ["曇り", " ☁ "], ["曇", " ☁ "], ["雨", " ☂ "], ["雪", " ☃ "]]
+  replaces.each{|e| h[:telop].gsub!(e[0],e[1])}
+  h
+end
+
+def message_day(h)
   "#{h[:date]}の天気:#{h[:telop]} 気温:#{h[:temperature_max]}℃/#{h[:temperature_min]}℃"
 end
 
@@ -35,8 +48,6 @@ url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
 # 地区ID: http://weather.livedoor.com/forecast/rss/primary_area.xml
 json_raw = open(url).read
 json = JSON.parse(json_raw)
-h = parse json
-
-p ImKayac.post(user, message(h), {:password => password})
-
-
+forecasts = parse(json)
+p message = forecasts.map{|e| decolate(e)}.map{|e| message_day(e)}.join("\n")
+p ImKayac.post(user, message, {:password => password})
